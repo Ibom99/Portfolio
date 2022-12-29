687ef230601beb54d4b014f8352c295681e00ce6
Ibom99 <ibom.esther@gmail.com>
2022-12-29 17:20:58 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".auto-type",{
+    strings: ["SOFTWARE ENGINEER.", "FRONT END DEVELOPER.","TECHNICAL WRITER."],
+    typeSpeed: 150,
+    backSpeed: 100,
+    loop: true
+})
+
